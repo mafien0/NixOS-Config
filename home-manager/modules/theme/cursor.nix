4b67ae407ce4b8619fd6d7f4a4006f2720a75efb
Mafien_0 <93920918+Mafien01@users.noreply.github.com>
2025-07-02 09:38:59 +0500

    configured cursor

--- home-manager/modules/theme/cursor.nix
+++ home-manager/modules/theme/cursor.nix
@@ -1,26 +1,9 @@
 { config, pkgs, ... }: {
-
-  home.packages = with pkgs; [
-    catppuccin-cursors.mochaLavender # Cursor
-  ];
-
+	
   home.pointerCursor = {
     enable = true; 
-    package = pkgs.catppuccin-cursors.mochaLavender;
-    name = "catppuccin-mocha-lavender-cursors";
+    package = pkgs.volantes-cursors;
+    name = "volantes_cursors";
     size = 24;
-  };
-
-  gtk = {
-    enable = true;
-    gtk2.extraConfig = "gtk-cursor-theme-name = \"catppuccin-mocha-lavender-cursors\";";
-
-    gtk3.extraConfig = {
-      gtk-cursor-theme-name = "catppuccin-mocha-lavender-cursors";
-    };
-
-    gtk4.extraConfig = {
-      gtk-cursor-theme-name = "catppuccin-mocha-lavender-cursors";
-    };
   };
 }
