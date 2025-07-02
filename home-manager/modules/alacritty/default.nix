597fadf0bcdc4e3ae8b7d590293fe7a8da87ccaf
Mafien_0 <93920918+Mafien01@users.noreply.github.com>
2025-07-02 18:42:26 +0500

    configured btop

--- home-manager/modules/alacritty/default.nix
+++ home-manager/modules/alacritty/default.nix
@@ -7,4 +7,12 @@
 		".config/alacritty/alacritty.toml".source = ./alacritty.toml;
 		".config/alacritty/theme.toml".source = ./theme.toml;
 	};
+
+	programs.btop = {
+		enable = true;
+
+		settings = {
+			color_theme = "gruvbox_dark";
+		};
+	};
 }
