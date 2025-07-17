c55c8fa86ae9bce032054d4ddb3b15b7b828d6f8
Mafien01 <mafien_0@tuta.io>
2025-07-17 16:49:20 +0500

    removed sudo from alieses

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -3,8 +3,8 @@
 	programs.fish = {
 		enable = true;
 		shellAliases = {
-			nrs = "sudo nixos-rebuild switch --flake ~/nix";
-			nrb = "sudo nixos-rebuild boot --flake ~/nix";
+			nrs = "nixos-rebuild switch --flake ~/nix";
+			nrb = "nixos-rebuild boot --flake ~/nix";
 			hms = "home-manager switch --flake ~/nix";
 
 			v = "nvim";
