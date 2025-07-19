3bed043ec123eed7401ba4eddd8eafd81c907846
Mafien01 <mafien_0@tuta.io>
2025-07-19 22:42:30 +0500

    configured swaync

--- home-manager/modules/theme/gtk.nix
+++ home-manager/modules/theme/gtk.nix
@@ -3,13 +3,17 @@
 		enable = true;
 		font.name = "Fira Code Medium 12";
 		theme = {
-			name = "Gruvbox-Dark";
-			package = pkgs.gruvbox-gtk-theme;
+			name = "Gruvbox-Yellow-Dark";
+			package = pkgs.gruvbox-gtk-theme.override {
+				themeVariants = [ "yellow" ];
+			};
 		};
 
 		iconTheme = {
 			name = "Gruvbox-Plus-Dark";
-			package = pkgs.gruvbox-plus-icons;
+			package = pkgs.gruvbox-plus-icons.override {
+				folder-color = "yellow";
+			};
 		};
 	};
 }
