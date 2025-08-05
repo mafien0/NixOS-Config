23f80725a75dad012cd60555f3143034efe2a759
Mafien01 <mafien_0@tuta.io>
2025-08-05 13:20:37 +0500

    white theme fix

--- home-manager/modules/theme/default.nix
+++ home-manager/modules/theme/default.nix
@@ -1,8 +1,17 @@
-{ lib, ... }: {
+{
 	imports = [
 		./cursor.nix
 		./gtk.nix
 		./qt5ct.nix
 		./defaultApps.nix
 	];
+
+	dconf = {
+		enable = true;
+		settings = {
+			"org/gnome/desktop/interface" = {
+				color-scheme = "prefer-dark";
+			};
+		};
+	};
 }
