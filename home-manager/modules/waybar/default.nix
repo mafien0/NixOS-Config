8d367a36d2ff83d19eaabff440a3b1f4358a3221
mafien0 <mafien_0@tuta.io>
2025-08-07 04:11:03 +0500

    waybar color fix

--- home-manager/modules/waybar/default.nix
+++ home-manager/modules/waybar/default.nix
@@ -13,8 +13,8 @@
 @define-color border                      #${config.stylix.base16Scheme.base03};
 @define-color text                        #${config.stylix.base16Scheme.base05};
 @define-color ws-focused                  #${config.stylix.base16Scheme.base0A};
-@define-color ws-active-text              #${config.stylix.base16Scheme.base07};
-@define-color ws-persistent-text          #${config.stylix.base16Scheme.base04};
+@define-color ws-active-text              #${config.stylix.base16Scheme.base05};
+@define-color ws-persistent-text          #${config.stylix.base16Scheme.base07};
 @define-color ws-urgent                   #${config.stylix.base16Scheme.base08};
 	'';
 }
