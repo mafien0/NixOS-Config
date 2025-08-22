bf9c36691715d017e2f1d4ad743e59fcb4f6398d
mafien0 <mafien_0@tuta.io>
2025-08-22 19:21:27 +0500

    installed gnome

--- nixos/modules/wayland.nix
+++ nixos/modules/wayland.nix
@@ -1,30 +1,13 @@
 { pkgs, ... }: {
 
-	programs.hyprland = {
-		enable = true;
-		xwayland.enable = true;
-		portalPackage = pkgs.xdg-desktop-portal-gtk;
+	services.xserver = {
+		displayManager.gdm.enable = true;
+		desktopManager.gnome.enable = true;
 	};
 
 	environment.systemPackages = with pkgs; [
-		alacritty
-
-		hyprpicker
-		grim
-		slurp
-		swaylock
-		swaybg
-		swaynotificationcenter
-		waypaper
-		waybar
-
-		gnome-keyring
 		cliphist
 		wl-clipboard
-
-		xdg-desktop-portal
-		xdg-desktop-portal-gtk
-		xdg-desktop-portal-hyprland
 	];
 
 	environment.sessionVariables = {
@@ -35,20 +18,6 @@
 		QT_QPA_PLATFORMTHEME = "qt5ct";
 		ELECTRON_OZONE_PLATFORM_HINT = "wayland";
 		XDG_SESSION_TYPE = "wayland";
-	};
-
-	xdg = {
-		portal = {
-			xdgOpenUsePortal = true;
-			config = {
-				common.default = [ "gtk" "hyprland" ];
-				hyprland.default = [ "gtk" "hyprland" ];
-			};
-			extraPortals = with pkgs; [
-				xdg-desktop-portal-gtk
-				xdg-desktop-portal-hyprland
-			];
-		};
 	};
 
 	xdg.mime.defaultApplications = {
