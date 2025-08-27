cc745711959eeb724d7b0b6daa1fe462ac7164a6
mafien0 <mafien_0@tuta.io>
2025-08-27 06:01:52 +0500

    configured fastfetch for neat screenshots

---
+++ home-manager/modules/cli/fastfetch.nix
@@ -0,0 +1,32 @@
+{
+	programs.fastfetch = {
+		enable = true;
+		settings = {
+			logo.source = "NixOS";
+			modules = [
+				"title"
+				"separator"
+
+				"os"
+				"host"
+				"kernel"
+				"uptime"
+				"separator"
+
+				"packages"
+				"de"
+				"terminal"
+				"shell"
+				"separator"
+
+				"theme"
+				"icons"
+				"cursor"
+				"terminalfont"
+				"separator"
+
+				"disk"
+			];
+		};
+	};
+}
